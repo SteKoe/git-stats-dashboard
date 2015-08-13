e1f74be3d9363092a961aa77851cec5273c5490c
unknown <j.sprenger@nb-sprenger.gbtecdb.local>
2015-08-13 22:11:07 +0200

    fills hotspotpackages in json tree

--- services/hotspotsPerPackage.js
+++ services/hotspotsPerPackage.js
@@ -6,6 +6,48 @@ var LinesPerFileService = require('./linesPerFile.js');
 module.exports = function (req, res) {
     return LinesPerFileService(req, res)
         .then(function (resp) {
-            console.log(resp);
+            return fillHotspotsInTree(resp);
         });
+
+    //TODO: redundant code, inject logik in Test/hotspotsPerPackageService.js
+    function fillHotspotsInTree(data) {
+        var tree = {};
+
+        function fillTree(name, steps) {
+            var current = null,
+                existing = null,
+                i = 0;
+            for (var y = 0; y < steps.length-1; y++) {
+                if (y == 0) {
+                    if (!tree.children || typeof tree.children == 'undefined') {
+                        tree = {name: steps[y], children: []};
+                    }
+                    current = tree.children;
+                } else {
+                    existing = null;
+                    for (i = 0; i < current.length; i++) {
+                        if (current[i].name === steps[y]) {
+                            existing = current[i];
+                            break;
+                        }
+                    }
+                    if (existing) {
+                        current = existing.children;
+                    } else {
+                        current.push({name: steps[y], children: []});
+                        current = current[current.length - 1].children;
+                    }
+                }
+            }
+            current.push({name: steps[steps.length-1], count: name})
+        }
+
+        for (var x=0; x < data.length; x++) {
+            var steps = data[x].name.split('/').splice(1);
+            if(steps.length > 1) {
+                fillTree(data[x].stats.total, steps)
+            }
+        }
+        return tree;
+    };
 };
--- test/services/hotspotsPerPackageService.js
+++ test/services/hotspotsPerPackageService.js
@@ -1,23 +1,53 @@
 'use strict';
 
-function identifyHotspots(resp) {
-    var result = {};
+function fillHotspotsInTree(data) {
+    var tree = {};
 
-    resp.forEach(function (i) {
-        result.name = i.name;
-        result.children = [];
-        result.count = i.stats.total;
-    });
-    return result;
+    function fillTree(name, steps) {
+        var current = null,
+            existing = null,
+            i = 0;
+        for (var y = 0; y < steps.length-1; y++) {
+            if (y == 0) {
+                if (!tree.children || typeof tree.children == 'undefined') {
+                    tree = {name: steps[y], children: []};
+                }
+                current = tree.children;
+            } else {
+                existing = null;
+                for (i = 0; i < current.length; i++) {
+                    if (current[i].name === steps[y]) {
+                        existing = current[i];
+                        break;
+                    }
+                }
+                if (existing) {
+                    current = existing.children;
+                } else {
+                    current.push({name: steps[y], children: []});
+                    current = current[current.length - 1].children;
+                }
+            }
+        }
+        current.push({name: steps[steps.length-1], count: name})
+    }
+
+    for (var x=0; x < data.length; x++) {
+        var steps = data[x].name.split('/').splice(1);
+        if(steps.length > 1) {
+            fillTree(data[x].stats.total, steps)
+        }
+    }
+    return tree;
 };
 
-xdescribe('hotspots per package test', function () {
+describe('hotspots per package test', function () {
     it('should transfrom to json tree structure', function (done) {
         var input = [{
             "name": "/d3.js",
             "stats": {"total": 235, "source": 199, "comment": 4, "single": 0, "block": 4, "mixed": 0, "empty": 32}
         }, {
-            "name": "/1/2/store.spec.js",
+            "name": "/1/2/store.js",
             "stats": {"total": 185, "source": 142, "comment": 11, "single": 11, "block": 0, "mixed": 7, "empty": 39}
         }, {
             "name": "/1/2/flore.js",
@@ -27,13 +57,15 @@ xdescribe('hotspots per package test', function () {
         var expected = {
             "name": "1",
             "children": [{
-                    "name": "2",
-                    "children": [
-                        {"name": "store.js", "count": 185},
-                        {"name": "flore.js", "count": 116}
-                ]}]};
+                "name": "2",
+                "children": [
+                    {"name": "store.js", "count": 185},
+                    {"name": "flore.js", "count": 116}
+                ]
+            }]
+        };
 
-        JSON.stringify(identifyHotspots(input)).should.equal(JSON.stringify(expected));
+        JSON.stringify(fillHotspotsInTree(input)).should.equal(JSON.stringify(expected));
         done();
     });
 });
