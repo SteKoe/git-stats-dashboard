9137064b6186936daf22b4fa6fe3e7e7510bfd22
Stephan Köninger <stephan.koeninger@gbtec.de>
2015-08-14 08:47:34 +0200

    chore(dashboard): adds some example metrics

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,5 +4,7 @@ angular.module('de.devjs.dashboard.git', [
     'de.devjs.dashboard.git.dashboard',
 
     // Widgets
-    'de.devjs.dashboard.git.widget.sample'
+    'de.devjs.dashboard.git.widget.sample',
+    'de.devjs.angular.d3circle',
+    'de.devjs.angular.d3pie'
 ]);
--- public/javascripts/dashboard/controller/dashboard.js
+++ public/javascripts/dashboard/controller/dashboard.js
@@ -1,16 +1,44 @@
 angular.module('de.devjs.dashboard.git.dashboard')
-    .controller('DashboardController', ['$scope', function($scope) {
-        $scope.standardItems = [
-            { sizeX: 2, sizeY: 1, row: 0, col: 0, style: 'color-cyan' },
-            { sizeX: 2, sizeY: 2, row: 0, col: 2, style: 'color-purple' },
-            { sizeX: 1, sizeY: 1, row: 0, col: 4, style: 'color-orange' },
-            { sizeX: 1, sizeY: 1, row: 0, col: 5, style: 'color-salmon' },
-            { sizeX: 2, sizeY: 1, row: 1, col: 0, style: 'color-cyan' },
-            { sizeX: 1, sizeY: 1, row: 1, col: 4, style: 'color-purple' },
-            { sizeX: 1, sizeY: 2, row: 1, col: 5, style: 'color-orange' },
-            { sizeX: 1, sizeY: 1, row: 2, col: 0, style: 'color-salmon' },
-            { sizeX: 2, sizeY: 1, row: 2, col: 1, style: 'color-cyan' },
-            { sizeX: 1, sizeY: 1, row: 2, col: 3, style: 'color-purple' },
-            { sizeX: 1, sizeY: 1, row: 2, col: 4, style: 'color-salmon' }
-        ];
+    .controller('DashboardController', ['$scope', '$http', function ($scope, $http) {
+        var url = "https://github.com/SteKoe/git-stats-dashboard";
+        var headers = {};
+
+        $scope.gridItems = {
+            loc: {sizeX: 2, sizeY: 3, row: 0, col: 0, style: 'color-salmon'},
+            committers: {sizeX: 2, sizeY: 2, row: 0, col: 2, style: 'color-cyan'},
+            langStat: {sizeX: 2, sizeY: 2, row: 0, col: 4, style: 'color-orange'},
+            commitsPerDay: {sizeX: 4, sizeY: 1, row: 3, col: 2, style: 'color-orange'}
+        };
+
+        $http.get('/git/committer/commits?url=' + url, headers)
+            .then(function (resp) {
+                $scope.committers = [];
+                resp.data.forEach(function (resp) {
+                    $scope.committers.push(resp);
+                });
+            });
+
+        $http.get('/git/file/types?url=' + url, headers)
+            .then(function (resp) {
+                $scope.langStat = [];
+                resp.data.forEach(function (resp) {
+                    $scope.langStat.push({
+                        name: Object.keys(resp)[0],
+                        count: Math.round(resp[Object.keys(resp)[0]] * 1000) / 10
+                    });
+                });
+
+                function getReadableTypeName(type) {
+
+                }
+            });
+
+        $http.get('/git/file/lines?url=' + url, headers)
+            .then(function (resp) {
+                resp.data = resp.data.filter(function (item) {
+                    // Filter out all vendors
+                    return item.name.indexOf('vendor') === -1;
+                }).splice(0, 20);
+                $scope.linesPerFile = resp.data;
+            });
     }]);
