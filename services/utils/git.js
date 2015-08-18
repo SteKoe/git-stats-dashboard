690bca5451ab277b48cb176d6608d3611fde98f1
Jonas Sprenger <sprengerj22@gmail.com>
2015-08-18 22:31:08 +0200

    bug(clonegitrepo): creates tmp folder if not exisits

--- services/utils/git.js
+++ services/utils/git.js
@@ -4,6 +4,7 @@
  * Module dependencies.
  */
 var fs = require('fs');
+var mkdirp = require('mkdirp');
 var Promise = require('bluebird');
 var Git = require('git-exec');
 var sha1 = require('sha1');
@@ -36,18 +37,22 @@ module.exports = (function () {
         console.log(path, procFile);
         procFile = './tmp/' + procFile;
 
+        mkdirp("./tmp/", function (err) {
+            if (err) console.error(err)
+        });
+
         var defered = Promise.defer();
 
-        if(!fs.existsSync(procFile)) {
+        if (!fs.existsSync(procFile)) {
             fs.writeFileSync(procFile, '1337');
             fs.chmodSync(procFile, '0777');
 
-            if(!fs.existsSync(targetPath)) {
+            if (!fs.existsSync(targetPath)) {
                 Git.clone(url, targetPath, function (repo) {
                     repo = new Git(targetPath);
 
-                    fs.unlink(procFile, function(err) {
-                        if(err) console.error(err);
+                    fs.unlink(procFile, function (err) {
+                        if (err) console.error(err);
 
                         console.log("done...");
                         defered.resolve(repo);
